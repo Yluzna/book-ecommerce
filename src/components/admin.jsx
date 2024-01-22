0ae52ee028be338fecb083a0dce8ca0a54a2cc4b
Yeshiprinss <martinezyessenia068@gmail.com>
2024-01-22 14:40:39 +0100

    add style in admin

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -3,14 +3,22 @@ import './Admin.css'
 const admin = () => {
   return (
     <>
-      <div className='title'>Panel de Administrador</div>
+      <h1 className='title'>Panel de Administrador</h1>
       <div className='search-panel'>
       <input
       type="text"
-      placeholder='Buscar'
+      placeholder='Search Book'
       className='text-panel'
       name='busqueda'> 
      </input> 
+     </div>
+     <div className='Articles'>
+      <img src="./img/juego-de-tronos.jpg" alt="img-libro" className='book-juegotronos'/>
+      <div className='description-article'>
+     <h2>Juego de Tronos</h2>
+     <p>GEORGE R.R MARTIN</p>
+     <p className='price'>$ 35.53</p>
+     </div>
      </div>
     </>
   )
