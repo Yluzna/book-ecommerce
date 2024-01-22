e096d2452ab0c0d36f1589bd03a11d04405bb9f7
VICTOR-M8 <darkhink8@gmail.com>
2024-01-22 09:10:08 +0100

    new change

---
+++ src/component/product/product.jsx
@@ -0,0 +1,14 @@
+import './product.css'
+import AddToCart from '../AddToCart/AddToCart'
+const Product = () => {
+    return (
+        <div className='product'>
+            <h3 className='product-calification'> <img src="./src/assets/img/starIcon.svg" alt="" />4.9</h3>
+            <img className='product-img' src="./src/assets/img/libro1.svg" alt="product" />
+            <h2 className='product-title'>Choque de Reyes</h2>
+            <h3 className='product-price'>$4.53 <AddToCart /></h3>
+        </div>
+    )
+}
+
+export default Product
