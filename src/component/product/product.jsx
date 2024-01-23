fb1fc43b4cf704034cd30574a2bf97b582c47feb
VICTOR-M8 <darkhink8@gmail.com>
2024-01-23 10:39:17 +0100

    changes

--- src/component/product/product.jsx
+++ src/component/product/product.jsx
@@ -3,19 +3,14 @@ import AddToCart from '../AddToCart/AddToCart'
 const Product = (props) => {
     return (
         <div className='product'>
-<<<<<<< HEAD
+
             <h3 className='product-calification'> <img src="./src/assets/img/starIcon.svg" alt="" />4.9</h3>
             <p className='product-add'>+</p>
             <img className='product-img' src="./src/assets/img/libro1.svg" alt="product" />
             <h2 className='product-title'>Choque de Reyes</h2>
             <h3 className='product-price'>$4.53 <AddToCart /></h3>
-            
-=======
-            <h3 className='product-calification'> <img src="./src/assets/img/starIcon.svg" alt="" /> {props.calification}</h3>
-            <img className='product-img' src="./src/assets/img/libro1.svg" alt="product"></img>
-            <h2 className='product-title'>{props.name}</h2>
-            <h3 className='product-price'>{props.price} <AddToCart /></h3>
->>>>>>> cdc2f57b4304db4b3840c3abd03d245d8f81025e
+
+
         </div>
     )
 }
