a6fbd144b5d74b1a26acab25ab5825ed80571b67
neymi-k <148432516+neymi-k@users.noreply.github.com>
2024-01-23 09:59:04 +0100

    add-product

--- src/component/product/product.jsx
+++ src/component/product/product.jsx
@@ -4,9 +4,11 @@ const Product = () => {
     return (
         <div className='product'>
             <h3 className='product-calification'> <img src="./src/assets/img/starIcon.svg" alt="" />4.9</h3>
+            <p className='product-add'>+</p>
             <img className='product-img' src="./src/assets/img/libro1.svg" alt="product" />
             <h2 className='product-title'>Choque de Reyes</h2>
             <h3 className='product-price'>$4.53 <AddToCart /></h3>
+            
         </div>
     )
 }
