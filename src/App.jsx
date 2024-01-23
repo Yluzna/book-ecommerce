83b3bbb3b64fbd12661aa3f076e41f807b3f2401
Yadyvivian <148440973+Yadyvivian@users.noreply.github.com>
2024-01-23 13:55:14 +0100

    prop in admin

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,20 @@ function App() {
 
   return (
     <>
-    <Admin />
+    <h1 className='title'>Panel de Administrador</h1>
+      <div className='search-panel'>
+      <input
+      type="text"
+      placeholder='Search Book'
+      className='text-panel'
+      name='busqueda'> 
+     </input> 
+     </div>
+    <Admin  img="../img/juego-de-tronos.jpg" price="$30" bookName="Juego de tronos" />
+    <Admin  img="../img/festin-de-cuervos-cancion-de-hielo-y-fuego.jpg" price="$35" bookName="Festin de cuervos cancion de hielo y fuego" />
+    <Admin  img="../img/choque-de-reyes-cancion-de-hielo-y-fuego.jpg" price="$30" bookName="Choque de reyes cancionde hielo y fuego" />
+    <Admin  img="../img/danza-de-dragones-cancion-de-hielo-y-fuego.jpg" price="$32" bookName="Danza de dragones cancion de hielo y fuego" />
+    <Admin  img="../img/tormenta-de-espadas-cancion-de-hielo-y-fuego.jpg" price="$32" bookName="Tormenta de espadas cancion de hielo y fuego." />
     </>
 
   )
--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -1,27 +1,19 @@
 import './Admin.css'
 
-const admin = () => {
+const Admin = (props) => {
   return (
     <>
-      <h1 className='title'>Panel de Administrador</h1>
-      <div className='search-panel'>
-      <input
-      type="text"
-      placeholder='Search Book'
-      className='text-panel'
-      name='busqueda'> 
-     </input> 
-     </div>
+      
+
      <div className='Articles'>
-      <img src="./img/juego-de-tronos.jpg./" alt="img-libro" className='book-juegotronos'/>
+      <img src={props.img} alt="img-libro" className='book-juegotronos'/>
       <div className='description-article'>
-     <h2>Juego de Tronos</h2>
+     <h2>{props.bookName}</h2>
      <p>GEORGE R.R MARTIN</p>
-     <p className='price'>$ 35.53</p>
+     <p className='price'>{props.price}</p>
      </div>
-     
      </div>
     </>
   )
    }
-export default admin
+export default Admin
